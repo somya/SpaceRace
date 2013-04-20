01765614dd0d3bb3ac064d9a19db0463829203bc
Somya Jain <somyaj@gmail.com>
2013-04-20 19:54:26 -0400

    initial commit

---
+++ js/game.js
@@ -0,0 +1,15 @@
+ 
+ function onCategory1Click()
+ {
+ 	alert("Clicked on category 1.");
+ }
+
+ function onCategory2Click()
+ {
+ 	alert("Clicked on category 2.");
+ }
+
+ function onCategory3Click()
+ {
+ 	alert("Clicked on category 3.");
+ }
